88d11416926b0376835577a2913f0392c6137b2e
minko <minko.thet@ufinity.com>
2020-09-14 22:40:44 +0800

    Setting up storybook with react typescript

---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import './button.css';
+
+export interface Props {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+const Button: FC<Props> = ({ primary = true, size = 'medium', backgroundColor, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
