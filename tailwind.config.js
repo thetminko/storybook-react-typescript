ad274648936c2bc126920f1336a11595f2de1799
minko <minko.thet@ufinity.com>
2020-09-21 10:40:01 +0800

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  // purge: ['./src/App.tsx', './src/components/**.tsx', './src/components/**/*.tsx', './src/containers/**/*.tsx', './src/pages/**/*.tsx'], // this will only purge if NODE_ENV is production
-  purge: { // this will force purge even in dev. For testing only
-    enabled: true,
-    content: ['./src/App.tsx', './src/components/**.tsx', './src/components/**/*.tsx', './src/containers/**/*.tsx', './src/pages/**/*.tsx']
-  },
+  purge: ['./src/App.tsx', './src/components/**.tsx', './src/components/**/*.tsx', './src/containers/**/*.tsx', './src/pages/**/*.tsx'], // this will only purge if NODE_ENV is production
+  // purge: { // this will force purge even in dev. For testing only
+  //   enabled: true,
+  //   content: ['./src/App.tsx', './src/components/**.tsx', './src/components/**/*.tsx', './src/containers/**/*.tsx', './src/pages/**/*.tsx']
+  // },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: [],
+
+      }
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus'] // allow hover, focus for background color, https://tailwindcss.com/docs/pseudo-class-variants
